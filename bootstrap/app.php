818306ceed8abac33b5ddc05aeb48106513e7bcf
Rick Lambrechts <rick@rl-webdiensten.nl>
2025-05-12 17:53:08 +0200

    Replace trustHosts middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,16 +10,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->use([
-            \Illuminate\Foundation\Http\Middleware\InvokeDeferredCallbacks::class,
-            \App\Http\Middleware\TrustHosts::class,
-            \Illuminate\Http\Middleware\TrustProxies::class,
-            \Illuminate\Http\Middleware\HandleCors::class,
-            \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
-            \Illuminate\Http\Middleware\ValidatePostSize::class,
-            \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
-            \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        ]);
+        $middleware->trustHosts(static function (): array {
+            return Config::array('app.trusted_hosts');
+        });
         $middleware->appendToGroup('web', [
             \App\Http\Middleware\Locale::class
         ]);
