c83658b4f413df8f17a5287d49a2f9f6cdb5596e
Rick Lambrechts <rick@rl-webdiensten.nl>
2025-05-15 17:48:37 +0200

    Update trust hosts

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Config;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustHosts(static function (): array {
             return Config::array('app.trusted_hosts');
-        });
+        }, subdomains: false);
         $middleware->appendToGroup('web', [
             \App\Http\Middleware\Locale::class
         ]);
