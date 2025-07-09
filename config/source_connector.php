aa23552197e3bd36a5a5e65922ed7c7ca6d90a62
Joshua Thijssen <jthijssen@noxlogic.nl>
2025-07-09 11:36:00 +0200

    Added configurable endpoint

--- config/source_connector.php
+++ config/source_connector.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
     'url' => env('SOURCE_CONNECTOR_URL', false),
-
+    'endpoint' => env('SOURCE_CONNECTOR_ENDPOINT', '/enrich'),
     'mtls_cert' => env('SOURCE_CONNECTOR_CERT', false),
     'mtls_key' => env('SOURCE_CONNECTOR_KEY', false),
     'mtls_cacert' => env('SOURCE_CONNECTOR_CACERT', false),
