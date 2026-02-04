c2793a9991ae842167bf044778b8c0252f91304e
Fouad Sfarijlani <fouad.sfarijlani@hotmail.com>
2026-02-04 14:11:10 +0100

    fix: hardcoded portnumber for wallet in local development

--- resources/views/auth/vc.blade.php
+++ resources/views/auth/vc.blade.php
@@ -36,7 +36,7 @@
                     </button>
                     <div aria-labelledby="debug-accordion">
                         @env('local')
-                            <p>Lokale wallet URL: http://localhost:8562/api/siop/initiatePresentation</p>
+                            <p>Lokale wallet URL: http://localhost:{{env('WALTID_DEV_WALLET_FRONTEND_PORT')}}/api/siop/initiatePresentation</p>
                         @endenv
                     </div>
                 </li>
